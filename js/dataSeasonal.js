2146f5357b743d1688f73fe79307e3a18094dd71
Mordasan Alexandru-Ioan <mordasanalex@yahoo.com>
2022-03-17 20:49:53 +0200

    added dataSeasonal.js & data - working on data(database)

---
+++ js/dataSeasonal.js
@@ -0,0 +1,61 @@
+const dataSeasonal = [{
+        prodName: {
+            type: "Shoes",
+            brand: "Nike",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "24.99",
+    },
+    {
+        prodName: {
+            type: "Hat",
+            brand: "Outhorn",
+        },
+        picture: "img/outhornHat.png",
+        rating: "&#9733; &#9733; &#9733; &#9734; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "11.38",
+    },
+    {
+        prodName: {
+            type: "Hoodie",
+            brand: "S.O.A.",
+        },
+        picture: "img/soaHoodie.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9733;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "37.49",
+    },
+    {
+        prodName: {
+            type: "Pants",
+            brand: "Army",
+        },
+        picture: "img/soaHoodie.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "20.00",
+    },
+    {
+        prodName: {
+            type: "Jacket",
+            brand: "H&M",
+        },
+        picture: "img/soaHoodie.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9733;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "32.00",
+    },
+    {
+        prodName: {
+            type: "Jacket",
+            brand: "H&M",
+        },
+        picture: "img/soaHoodie.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9733;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "32.00",
+    },
+];
