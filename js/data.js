b2753899d025cd44812f16a3f4f19e088e689866
Mordasan Alexandru-Ioan <mordasanalex@yahoo.com>
2022-03-18 20:29:00 +0200

    worked on data.js & downloaded photos for data.js items

--- js/data.js
+++ js/data.js
@@ -11,6 +11,336 @@ const data = [{
     {
         prodName: {
             type: "Shoes",
+            brand: "Adidas",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "Adidas",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "Adidas",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "Adidas",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "Adidas",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "Adidas",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "Adidas",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "Adidas",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "Adidas",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "Zara",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "Zara",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "Zara",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "Zara",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "Zara",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "Zara",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "H&M",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "H&M",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "H&M",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "H&M",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Pants",
+            brand: "Pull&Bear",
+        },
+        picture: "img/soaHoodie.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "20.00",
+    },
+    {
+        prodName: {
+            type: "Pants",
+            brand: "Pull&Bear",
+        },
+        picture: "img/soaHoodie.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "20.00",
+    },
+    {
+        prodName: {
+            type: "Pants",
+            brand: "Pull&Bear",
+        },
+        picture: "img/soaHoodie.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "20.00",
+    },
+    {
+        prodName: {
+            type: "Pants",
+            brand: "Pull&Bear",
+        },
+        picture: "img/soaHoodie.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "20.00",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "H&M",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "Zara",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "Asisc",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "Prada",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "Vans",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "10.00",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "Vans",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "8.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "Levi's",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "Puma",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "Zara",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
+            brand: "Adidas",
+        },
+        picture: "img/nikeShoes.png",
+        rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
+        description: "Description Lorem ipsum dolor sit amet consectetur.",
+        price: "19.99",
+    },
+    {
+        prodName: {
+            type: "Shoes",
             brand: "Nike",
         },
         picture: "img/nikeShoes.png",
@@ -41,7 +371,7 @@ const data = [{
     {
         prodName: {
             type: "Pants",
-            brand: "Army",
+            brand: "Pull&Bear",
         },
         picture: "img/soaHoodie.png",
         rating: "&#9733; &#9733; &#9733; &#9733; &#9734;",
